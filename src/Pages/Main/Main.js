629f17229ee9fb5a34916f881cc2a94473c64250
nubelsondev <nubelsondev@hotmail.com>
2020-01-11 14:25:39 +0000

    feat: Main page

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../services/api'
 
-const Main = () => {
-	return <h1>Main Page</h1>
+import './Main.scss'
+
+import logo from '../../assets/logo.svg'
+import like from '../../assets/like.svg'
+import dislike from '../../assets/dislike.svg'
+
+const Main = ({ match }) => {
+	const [devs, setDevs] = useState([])
+
+	useEffect(() => {
+		async function loadUsers() {
+			const response = await api.get('/devs', {
+				headers: {
+					user: match.params.id
+				}
+			})
+
+			setDevs(response.data)
+		}
+
+		loadUsers()
+	}, [match.params.id])
+
+	return (
+		<div className='main-container'>
+			<img src={logo} alt='Tindev' />
+
+			<ul>
+				{devs.map(dev => (
+					<li key={dev._id}>
+						<img src={dev.avatar} alt='' />
+						<footer>
+							<strong>{dev.name}</strong>
+							<p>{dev.bio}</p>
+						</footer>
+
+						<div className='buttons'>
+							<button type='button'>
+								{' '}
+								<img src={dislike} alt='Dislike' />{' '}
+							</button>
+							<button type='button'>
+								{' '}
+								<img src={like} alt='Like' />{' '}
+							</button>
+						</div>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default Main
